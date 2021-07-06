a1d2aa5e13ed86f65bd9b628dc7a919fb5239d88
rifat <rifatalmamun.cse@gmail.com>
2021-07-06 13:32:18 +0600

    url switching

--- first_app/views.py
+++ first_app/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 # here is my first django comment
+
+def welcome(request):
+    return HttpResponse("<h3>Welcome Django</h3> <a href='/home'>Home</a>")
+
+def index(request):
+    return HttpResponse("Hello rifat")
+
+def home(request):
+    return HttpResponse("<h3>Home</h3> <a href='/'>Welcome</a>")
